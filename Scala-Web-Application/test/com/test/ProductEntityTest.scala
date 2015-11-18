7907069a5d0cb9af536dddb7e2168a696d1fa10b
CallumCooper <Callum.Cooper@qa.com>
2015-11-18 14:22:12 +0000

    More entity tests

--- Scala-Web-Application/test/com/test/ProductEntityTest.scala
+++ Scala-Web-Application/test/com/test/ProductEntityTest.scala
@@ -1,5 +1,7 @@
 package com.test
 
+import model.ProductEntity
+
 /**
   * Created by callum on 18/11/2015.
   */
@@ -9,7 +11,40 @@ class ProductEntityTest extends TestBase{
     * Test Product ID
     */
   def productEntityProductIDTest(): Unit ={
-    "By initialising "
+    "By initialising a Product ID it" should "set a Product ID with a valid integer" in
+    {
+      // Create an object and run test
+      val productEntity : ProductEntity = new ProductEntity(1, null, null)
+      productEntity.productID should be (1)
+    }
   }
 
+  /**
+    * Test Product Name
+    */
+  def productEntityProductNameTest(): Unit ={
+    "By initialising a Product Name it" should "set a Product Name with a valid string" in
+    {
+      // Create an object and run test
+      val productEntity : ProductEntity = new ProductEntity(0, "GardenGnome", null)
+      productEntity.productName should be ("GardenGnome")
+    }
+  }
+
+  /**
+    * Test Product Description
+    */
+  def productEntityProductName(): Unit ={
+    "By initialising a Product Description it" should "set a Product Description with a valid string" in
+    {
+      // Create an object and run test
+      val productEntity : ProductEntity = new ProductEntity(0, null, "A small garden gnome")
+      productEntity.productDescription should be ("A small garden gnome")
+    }
+  }
+
+  // Run tests
+  productEntityProductIDTest
+  productEntityProductNameTest
+  productEntityProductName
 }
